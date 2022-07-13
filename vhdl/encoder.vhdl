e3c4eaa99d1376236fedbcf9810d00a92ee93ab6
CYM <cym9786@gmail.com>
2022-07-13 18:25:31 +0000

    initial

---
+++ vhdl/encoder.vhdl
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.consts.all;
+
+entity encoder is
+  port (
+    msg : in bit_vector(MSG_LENGTH to 0);
+    -- generator matrix
+    g : in generator_matrix;
+    encoded : out bit_vector(MSG_LENGTH to 0)
+  );
+end encoder;
+
+architecture Encoder of encoder is
+  component MatrixTransposer
+    port (
+      input : in code_matrix;
+      output : out code_matrix_transpose
+    );
+  end component;
+begin
+
+end architecture;
---
+++ vhdl/matrix_transposer.vhdl
@@ -0,0 +1,28 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.consts.ALL;
+
+ENTITY matrix_transposer IS
+    PORT (
+        input : IN code_matrix;
+        output : OUT code_matrix_transpose;
+        start : IN STD_LOGIC;
+        finish : OUT STD_LOGIC
+    );
+END matrix_transposer;
+
+ARCHITECTURE MatrixTransposer OF matrix_transposer IS
+BEGIN
+
+    PROCESS (input)
+        VARIABLE temp : code_matrix_transpose;
+    BEGIN
+        FOR I IN 0 TO CODEWORD_LENGTH LOOP
+            FOR J IN 0 TO MSG_LENGTH LOOP
+                temp(J, I) := input(I, J);
+            END LOOP;
+        END LOOP;
+        output <= temp;
+    END PROCESS;
+END ARCHITECTURE;
