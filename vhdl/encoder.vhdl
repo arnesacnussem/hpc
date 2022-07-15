c6eba07a4c5056a0a496d8e8422eaa8d48657e6c
Arne <arnesacnussem@users.noreply.github.com>
2022-07-15 15:45:06 +0000

    5

--- vhdl/encoder.vhdl
+++ vhdl/encoder.vhdl
@@ -6,8 +6,8 @@ ENTITY encoder IS
   PORT (
     msg : IN bit_vector(MSG_LENGTH TO 0);
     -- generator matrix
-    g : IN generator_matrix;
-    encoded : OUT bit_vector(MSG_LENGTH TO 0)
+    gen : IN generator_matrix;
+    encoded : OUT bit_vector(CODEWORD_LENGTH TO 0)
   );
 END encoder;
 
@@ -18,9 +18,34 @@ ARCHITECTURE Encoder OF encoder IS
       output : OUT code_matrix_transpose
     );
   END COMPONENT;
-BEGIN
-  PROCESS
+  PROCEDURE matrix_multiplex_add(
+    VARIABLE a, b : IN BIT;
+    VARIABLE c : OUT BIT
+  ) IS
   BEGIN
+    IF a = '1' AND b = '1' THEN
+      c := '0';
+    ELSIF a = '0' AND b = '0' THEN
+      c := '0';
+    ELSE
+      c := '1';
+    END IF;
+  END matrix_multiplex_add;
+BEGIN
+  PROCESS (msg, gen)
+    VARIABLE temp : bit_vector(CODEWORD_LENGTH DOWNTO 0);
+    VARIABLE m, g, t : BIT;
 
+  BEGIN
+    temp := (0 => '0', OTHERS => '0');
+    FOR col IN CODEWORD_LENGTH - 1 TO 0 LOOP
+      FOR row IN MSG_LENGTH - 1 TO 0 LOOP
+        m := msg(row);
+        g := gen(col, row);
+        matrix_multiplex_add(a => m, b => g, c => t);
+
+      END LOOP;
+      REPORT "value of " & INTEGER'image(col) & "=" & BIT'image(temp(col));
+    END LOOP;
   END PROCESS;
 END ARCHITECTURE;
