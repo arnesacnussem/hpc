00c2f3109d3fefc4538d084fb9ff1b88c255b8d3
Arne <arnesacnussem@users.noreply.github.com>
2022-07-19 02:29:55 +0000

    use package.json to manage workspace

--- gen_consts.js
+++ gen_consts.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const path = require("path");
-const { exec } = require("child_process");
+import { writeFileSync } from "fs";
+import { resolve } from "path";
+import { exec } from "child_process";
 const commentPrefix = "--\t";
 const comment = (m) => commentPrefix + m;
-const chk_bits = process.argv.length === 2 ? 4 : parseInt(process.argv[2]);
+const chk_bits = process.argv.length === 2 ? 3 : parseInt(process.argv[2]);
 console.log(`Generating constants for ${chk_bits}-bit parity check.`);
 exec(
   `octave-cli -q --eval '
@@ -73,7 +73,7 @@ PACKAGE consts IS
     );
 END PACKAGE consts;
 `;
-    fs.writeFileSync(path.resolve("vhdl/consts.vhdl"), vhdl);
+    writeFileSync(resolve("vhdl/consts.vhdl"), vhdl);
     console.log(`Generated constants for ${chk_bits}-bit parity check.`);
   }
 );
