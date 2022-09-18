71b23dd19f356e50cf1f03fd952bae7ca7573af2
CYM <cym9786@gmail.com>
2022-09-18 13:34:22 +0000

    move static tool setup

--- tester/setupStaticTools.sh
+++ tester/setupStaticTools.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
-
-sudo apt update
-sudo apt-get install -y gnupg2 curl software-properties-common
-curl https://packages.grafana.com/gpg.key | sudo apt-key add -
-sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
-sudo apt-get update
-sudo apt-get -y install grafana prometheus prometheus-pushgateway
-
-
 sudo cp -f "$BASEDIR/grafana.ini" /etc/grafana/grafana.ini
 sudo cp -f "$BASEDIR/prometheus.yml" /etc/prometheus/prometheus.yml
 sudo mkdir -p /var/run/prometheus/
