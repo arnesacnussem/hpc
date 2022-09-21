bbde08d56a7a76727497945a2fa545d7d86d0d9a
CYM <cym9786@gmail.com>
2022-09-21 10:18:30 +0000

    use topo sort for hierarchy

--- scripts/hierarchy.ts
+++ scripts/hierarchy.ts
@@ -47,42 +47,99 @@ type VHDLFile2 = {
   [key: string]: VHDLFile & { dependBy: string[] };
 };
 
-// this is like doing a reverse of a DAG: dependBy
-const buildDepBy = (dict: VHDLDict, list: VHDLFile[]) => {
-  const rdag = {} as VHDLFile2;
-  // create the dependBy map
-  list.forEach((vhdl) => {
-    const dpBy = list
-      .filter(
-        (vl) =>
-          vl.packageDeps.includes(vhdl.name) ||
-          vl.entityDeps.includes(vhdl.entity)
-      )
-      .map((v) => v.name);
-    rdag[vhdl.name] = {
-      ...vhdl,
-      dependBy: dpBy,
-    };
-  });
 
-  // update prior of each
-  const fileList = Object.keys(rdag)
-    .sort((a, b) => {
-      if (
-        rdag[a].dependBy.includes(rdag[b].name) ||
-        rdag[a].dependBy.includes(rdag[b].entity)
-      )
-        return -1;
-      else return 1;
+class Graph{
+  V: number;
+  adj: number[][];
+ 
+  // Constructor
+  constructor(v:number)
+  {
+      // Number of vertices
+      this.V = v
+
+      // Adjacency List as ArrayList of ArrayList's
+      this.adj = new Array(this.V)
+      for (let i = 0 ; i < this.V ; i+=1){
+          this.adj[i] = new Array()
+      }
+  }
+
+  // Function to add an edge into the graph
+  addEdge(v: number, w: number){
+      this.adj[v].push(w)
+  }
+
+  // A recursive function used by topologicalSort
+  topologicalSortUtil(v: number, visited: boolean[], stack: number[])
+  {
+      // Mark the current node as visited.
+      visited[v] = true;
+      let i = 0;
+
+      // Recur for all the vertices adjacent
+      // to thisvertex
+      for(i = 0 ; i < this.adj[v].length ; i++){
+          if(!visited[this.adj[v][i]]){
+              this.topologicalSortUtil(this.adj[v][i], visited, stack)
+          }
+      }
+
+      // Push current vertex to stack
+      // which stores result
+      stack.push(v);
+  }
+
+  // The function to do Topological Sort.
+  // It uses recursive topologicalSortUtil()
+  topologicalSort()
+  {
+      let stack = new Array()
+
+      // Mark all the vertices as not visited
+      let visited = new Array(this.V);
+      for (let i = 0 ; i < this.V ; i++){
+          visited[i] = false;
+      }
+
+      // Call the recursive helper
+      // function to store
+      // Topological Sort starting
+      // from all vertices one by one
+      for (let i = 0 ; i < this.V ; i++){
+          if (visited[i] == false){
+              this.topologicalSortUtil(i, visited, stack);
+          }
+      }
+
+      return stack
+  }
+}
+
+
+const topologicSort = (dict:VHDLDict)=>{
+  const keyList = Object.keys(dict)
+  const g = new Graph(keyList.length);
+  for (let index = 0; index < keyList.length; index++) {
+    const element = dict[keyList[index]];
+    element.entityDeps.forEach(v=>{
+      g.addEdge(index, keyList.indexOf(v))
     })
-    .map((k) => rdag[k]);
+    element.packageDeps.forEach(v=>{
+      g.addEdge(index,keyList.indexOf(v))
+    })
+  }
+  
+  const rdag = g.topologicalSort().map((i) => dict[keyList[i]]);
   console.log(
     "Sorted: ",
-    fileList.map((f) => f.name)
+    rdag.map((v) => v.name)
   );
+  return rdag;
+}
 
-  return fileList;
-};
+// use tolologic sort build rDAG
+
 
 // readFile("./vhdl/test/encoder_tb.vhdl");
 // readFile("./vhdl/gen/config.vhdl");
@@ -91,14 +148,10 @@ const dict = list.reduce((p, c) => {
   p[c.name] = c;
   return p;
 }, {} as VHDLDict);
-const nList = buildDepBy(dict, list);
-console.log("\n\n====DependBy====");
+const rdag = topologicSort(dict);
 
-nList.forEach((n) => {
-  console.log(`${n.name} >> ${n.dependBy.join(", ")}`);
-});
 fs.writeFileSync(
   "build/.list",
-  nList.map((n) => path.resolve(n.path)).join(" ")
+  rdag.map((n) => path.resolve(n.path)).join(" ")
 );
 console.log("Generated hierarchy list");
