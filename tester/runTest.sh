8ac0310f743a7368786fe6501ebc04276584b308
CYM <cym9786@gmail.com>
2022-09-23 15:55:36 +0000

    update setup script

--- tester/runTest.sh
+++ tester/runTest.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-bash setupStaticTools.sh
 pip install -r ../requirement.txt
 python main.py
--- tester/setupStaticTools.sh
+++ tester/setupStaticTools.sh
@@ -3,29 +3,38 @@
 BASEDIR=$(dirname "$0")
 sudo cp -f "$BASEDIR/grafana.ini" /etc/grafana/grafana.ini
 sudo cp -f "$BASEDIR/prometheus.yml" /etc/prometheus/prometheus.yml
-sudo mkdir -p /var/run/prometheus/
+
+mkdir prometheus_data
+sudo ln prometheus_data /var/lib/prometheus/
+sudo mkdir -p /var/run/prometheus
 sudo chmod 777 /var/run/prometheus/
-sudo service grafana-server start
+sudo service grafana-server start &
 sudo service prometheus restart
 sudo service prometheus-pushgateway restart
 
-echo "Creating grafana resources..."
-curl --silent --location -g --request POST 'http://localhost:3000/api/datasources' \
-    --header 'Accept: application/json' \
-    --header 'Content-Type: application/json' \
-    --data-raw '{
-    "name": "Prometheus",
-    "type": "prometheus",
-    "isDefault": true,
-    "url": "http://localhost:9090",
-    "access": "proxy"
-}'
-echo
-
-dsUid="$(curl --silent --location -g --request GET 'http://localhost:3000/api/datasources' \
-    --header 'Accept: application/json' \
-    --header 'Content-Type: application/json' | jq -r '.[0].uid')"
-echo
+echo -n "Creating grafana resources..."
+dsUid=
+while [ -z "$dsUid" ]
+do
+    curl --silent --location -g --request POST 'http://localhost:3000/api/datasources' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+        "name": "Prometheus",
+        "type": "prometheus",
+        "isDefault": true,
+        "url": "http://localhost:9090",
+        "access": "proxy"
+    }'
+    
+    dsUid="$(curl --silent --location -g --request GET 'http://localhost:3000/api/datasources' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' | jq -r '.[0].uid')"
+    
+    echo -n "."
+    sleep 1
+done
+echo "ok"
 
 curl --silent --location -g --request POST 'http://localhost:3000/api/dashboards/db' \
     --header 'Accept: application/json' \
