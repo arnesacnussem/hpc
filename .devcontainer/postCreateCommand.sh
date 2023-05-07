b9ad7e7b5fa222ff5f6f9f83b13829c03fa0a3dd
arnesacnussem <arnesacnussem@gmail.com>
2023-05-07 10:52:50 +0000

    disable tailscale

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,4 +3,4 @@
 rm ~/.prj_config.teros || ln -s /workspaces/hpc/.prj_config.teros ~/.prj_config.teros
 yarn install
 pip install -r requirement.txt
-sudo tailscale up --accept-routes --authkey "$TSKEY_AUTH"
+# sudo tailscale up --accept-routes --authkey "$TSKEY_AUTH"
